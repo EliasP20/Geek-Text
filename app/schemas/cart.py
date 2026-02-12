023eedb73e152c8a566bee204b9029b7fbdf9422
JopoPopo <jonathanpow05@gmail.com>
2026-02-12 12:16:04 -0500

    Added comments and fixed a bug when requesting cart contents

--- app/schemas/cart.py
+++ app/schemas/cart.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 from app.schemas.book import BookResponse
 
-# Used to read the items within a given cart, used in tandem with other schemas
 class CartItemRead(BaseModel):
     quantity: int
     book: BookResponse
@@ -9,16 +8,14 @@ class CartItemRead(BaseModel):
     class Config:
         from_attributes = True
 
-# Used for 'Retrieve the list of book(s) in the user's shopping cart'
 class CartResponse(BaseModel):
     user_id: int
-    user_name: str # Matches the 'user_name' variable in your router
-    items: list[CartItemRead] # Renamed 'books' to 'items' to match your model
+    user_name: str
+    items: list[CartItemRead]
 
     class Config:
         from_attributes = True
 
-# Used for 'Retrieve the subtotal price'
 class CartSubtotalResponse(BaseModel):
     user_id: int
     subtotal: float
