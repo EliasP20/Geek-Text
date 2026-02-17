b5432da52a0b36f38291db1a786b659db41a079c
Jplummer91 <133827427+Jplummer91@users.noreply.github.com>
2026-02-17 19:24:35 +0000

    Synced branch with main and resolved conflicts keeping main changes

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.api.routers.users import router as users_router
 from app.api.routers.ratings import router as ratings_router
+from app.api.routers.wishlist import router as wishlists_router
 
 app = FastAPI(title="Geek Text API")
 
 app.include_router(users_router)
 app.include_router(ratings_router)
+app.include_router(wishlists_router)
 
 Base.metadata.create_all(bind=engine)
 
